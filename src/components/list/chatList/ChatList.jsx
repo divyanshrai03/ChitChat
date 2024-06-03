1c7196eb272ed92a455f15e110a290e2bcc7e410
Divyansh Rai <113722329+divyanshrai03@users.noreply.github.com>
2024-06-03 23:04:04 +0530

    ChatList.jsx

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -12,9 +12,59 @@ const ChatList = () => {
         <img src={addMode ? "./minus.png" : './plus.png' }alt='' className='add' 
         onClick={() => setAddMode((prev) => !prev)}/>
         </div>
-        
+        <div className="item">
+          <img src='./avatar.png' alt=''/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src='./avatar.png' alt=''/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src='./avatar.png' alt=''/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src='./avatar.png' alt=''/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src='./avatar.png' alt=''/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src='./avatar.png' alt=''/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src='./avatar.png' alt=''/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
       </div>    
      )
+
+    
 }
 
 export default ChatList
