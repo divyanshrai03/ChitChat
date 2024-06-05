78549147c9795c66d66709633f645d6a5d793c8e
Divyansh Rai <113722329+divyanshrai03@users.noreply.github.com>
2024-06-05 12:02:18 +0530

    Detail.jsx

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,71 @@ import "./detail.css"
 
 const Detail = () => {
     return (
-        <div className="detail">Detail</div>
+        <div className="detail">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Lorem ipsum dolor sit, amet.</p>
+            </div>
+
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Setting</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & Help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <button>Block User  </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
